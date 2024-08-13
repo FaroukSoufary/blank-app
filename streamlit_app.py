a27314650d98c60185254129fb2b704d08b7758a
Farouk Soufary <113132058+FaroukSoufary@users.noreply.github.com>
2024-08-13 14:03:38 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import snowflake.connector
+
 
 # Write directly to the app
 st.title("Stackoverflow Weekly Programming Newsletter :computer:")
@@ -9,24 +9,10 @@ st.write(
 )
 
 # Get the current credentials
-
-conn = snowflake.connector.connect(
-    user='fsoufary',
-    password='$(echoLiiamra1)',
-    account='lm93303.eu-central-1',
-    warehouse='compute_wh',
-    database='pfe2024',
-    schema='stackoverflow'
-)
-cursor = conn.cursor()
-
-
-# Execute the query and fetch the data into a Pandas DataFrame
-query = "SELECT * FROM newsletters"
-cursor.execute(query)
-table_pandas_df = cursor.fetch_pandas_all()
-
-res_text = table_pandas_df.loc[table_pandas_df['CREATION_DATE'].idxmax()]['NEWSLETTER_BODY']
+conn = st.connection("snowflake")
+df = conn.query("select * from newsletters")
+st.dataframe(df)
+res_text = df.loc[df['CREATION_DATE'].idxmax()]['NEWSLETTER_BODY']
 st.markdown(res_text)
 
 
