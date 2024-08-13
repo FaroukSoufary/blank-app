93774d09523ea8cb3669dd6d6ec263729990e5ea
Farouk Soufary <113132058+FaroukSoufary@users.noreply.github.com>
2024-08-13 14:30:56 +0000

    update5

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
 
 # Get the current credentials
 # conn = st.connection("snowflake")
-conn = st.connection("conn1", type="snowflake")
+conn = st.connection("snowflake")
 df = conn.query("select * from newsletters")
 # st.dataframe(df)
 res_text = df.loc[df['CREATION_DATE'].idxmax()]['NEWSLETTER_BODY']
